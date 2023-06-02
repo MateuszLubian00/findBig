289b275910bce86ea134d2bc51b8088cab0ca27a
Mateusz Lubian <mickiewiczpoetafilantrop@gmail.com>
2023-06-02 09:39:44 +0200

    Formatted printing and included unit conversion for larger files.

--- src/Logic.java
+++ src/Logic.java
@@ -5,18 +5,33 @@
 import java.util.*;
 
 public class Logic {
+    /* List of unit names converted from B to nB. */
+    private static final String[] unitsNB = {"B", "KB", "MB", "GB", "TB"};
+    /* List of unit names converted from B to niB. */
+    private static final String[] unitsNiB = {"B", "KiB", "MiB", "GiB", "TiB"};
 
     /* Creates one map of files. */
     public static TreeSet<Map.Entry<String, Long>> listFiles(boolean recursion, boolean descOrder) {
         TreeSet<Map.Entry<String, Long>> filesMap;
-        filesMap = FileOp.readFiles("/test/test files", recursion, descOrder);
+        filesMap = FileOp.readFiles("", recursion, descOrder);
 
-        System.out.print(filesMap);
+        printFiles(filesMap);
         return filesMap;
     }
 
     /* Print out the list of files. */
-    private static void printFiles(TreeSet<Map.Entry<String, Long>> filesMap, boolean descOrder) {
-        System.out.println(filesMap);
+    private static void printFiles(TreeSet<Map.Entry<String, Long>> filesMap) {
+        int unit = 0;
+        float size;
+        for (Map.Entry file : filesMap) {
+            size = Float.parseFloat(file.getValue().toString());
+            while (size >= 1024) {
+                unit++;
+                size = size / 1024;
+            }
+
+            System.out.printf("%s - %.2f %s\n", file.getKey(), size, unitsNB[unit]);
+            unit = 0;
+        }
     }
 }
